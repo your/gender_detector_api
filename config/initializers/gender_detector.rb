487f51aa497ffaa5322bef25e69886b869ce6467
your <g.lobraico@gmail.com>
2016-04-13 16:45:42 +0200

    Edited gender_detector initializer

--- config/initializers/gender_detector.rb
+++ config/initializers/gender_detector.rb
@@ -1,10 +1,17 @@
 require 'gender_detector'
 
-DEFAULT_COUNTRY = :italy
+DEFAULT_COUNTRY   = :italy
+ALLOWED_COUNTRIES = GenderDetector::COUNTRIES
 
 class GenderDetector
+  FALLBACK_GENDER = 'f'
+
   def get(*args)
-    get_gender(*args)
+    case get_gender(*args)
+      when :male then 'm'
+      when :female then 'f'
+      else FALLBACK_GENDER
+    end
   end
 end
 
