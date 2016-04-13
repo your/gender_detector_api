dd69099c4c057c4a4d37b48988896835fb094bf5
your <g.lobraico@gmail.com>
2016-04-13 16:46:55 +0200

    Added super basic auth system via X-Client-Token header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+
+  before_filter :authenticate!
+
+  def authenticate!
+    unless APP_AUTH['auth']['tokens'].include?(request.headers['X-Client-Token'])
+      render nothing: true, status: 403
+    end
+  end
 end
---
+++ config/initializers/_load_auth.rb
@@ -0,0 +1,1 @@
+APP_AUTH = YAML.load_file("#{Rails.root.to_s}/config/auth.yml")
